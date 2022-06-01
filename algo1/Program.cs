73d6aac5c0c0363b7b147c9e640b7595ee19e22e
realtemirov <temirovjahongir2809@gmail.com>
2022-06-01 15:51:03 +0500

    change

---
+++ algo1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using VideoLibrary;
+
+
+namespace algo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }   
+    }
+}
